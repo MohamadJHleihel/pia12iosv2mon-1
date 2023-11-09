d61beb5866a000860bbb0ac50e086d5d547048cb
Mohamad Hleihel <mohammad.helehel@hotmail.com>
2023-11-09 19:48:08 +0100

    Initial Commit

---
+++ pia12iosv2mon-1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pia12iosv2mon-1
+//
+//  Created by Mohamad Hleihel on 2023-11-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pia12iosv2mon-1/pia12iosv2mon_1App.swift
@@ -0,0 +1,17 @@
+//
+//  pia12iosv2mon_1App.swift
+//  pia12iosv2mon-1
+//
+//  Created by Mohamad Hleihel on 2023-11-09.
+//
+
+import SwiftUI
+
+@main
+struct pia12iosv2mon_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
